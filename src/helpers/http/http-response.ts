eb99bd220341f17f082b834e132c8369737fc6a8
Gabriel dos Santos Oliveira <gabrielsantosoliveira951@gmail.com>
2023-03-30 14:25:54 -0400

    feat: add helper HttpResponse

---
+++ src/helpers/http/http-response.ts
@@ -0,0 +1,60 @@
+import { IHttpResponse } from '@/interface/index';
+
+import { ServerError, UnauthorizedError } from '../errors';
+import { HttpStatus } from './';
+export class HttpResponse {
+  static ok(body: unknown): IHttpResponse {
+    return {
+      body,
+      statusCode: HttpStatus.OK,
+    };
+  }
+  static serverError(): IHttpResponse {
+    return {
+      statusCode: HttpStatus.SERVER_ERROR,
+      body: { error: new ServerError().message },
+    };
+  }
+  static unauthorizedError(): IHttpResponse {
+    return {
+      statusCode: HttpStatus.UNAUTHORIZED_ERROR,
+      body: { error: new UnauthorizedError().message },
+    };
+  }
+  static notFound(error: Error): IHttpResponse {
+    return {
+      body: { error },
+      statusCode: HttpStatus.NOT_FOUND,
+    };
+  }
+  static noContent(): IHttpResponse {
+    return {
+      body: undefined,
+      statusCode: HttpStatus.NO_CONTENT,
+    };
+  }
+  static created(body?: unknown): IHttpResponse {
+    return {
+      body,
+      statusCode: HttpStatus.CREATED,
+    };
+  }
+  static conflict(error: Error): IHttpResponse {
+    return {
+      body: { error: error.message },
+      statusCode: HttpStatus.CONFLICT,
+    };
+  }
+  static forbidden(error: Error): IHttpResponse {
+    return {
+      body: { error: error.message },
+      statusCode: HttpStatus.FORBIDDEN,
+    };
+  }
+  static badRequest(error: Error): IHttpResponse {
+    return {
+      body: { error: error.message },
+      statusCode: HttpStatus.BAD_REQUEST,
+    };
+  }
+}
