7827c4fd9b7c66623791709422ef4ba39c519964
Gabriel dos Santos Oliveira <gabrielsantosoliveira951@gmail.com>
2023-04-02 12:13:56 -0400

    feat: add authService in lib

---
+++ src/main/lib/cryptography/auth-service.ts
@@ -0,0 +1,16 @@
+import { AuthService } from '@/infra/auth';
+import { JwtAdapter } from '@/infra/cryptography';
+import { env } from '@/main/config/env';
+
+const jwtAdapter = new JwtAdapter();
+const ONE_MINUTE = 60;
+const ONE_WEEK = 60 * 60 * 24 * 7;
+const authService = new AuthService({
+  decrypt: jwtAdapter,
+  encrypt: jwtAdapter,
+  expirationAccessToken: ONE_MINUTE,
+  expirationRefreshToken: ONE_WEEK,
+  secretAccessToken: env.SECRET_ACCESS_TOKEN,
+  secretRefreshToken: env.SECRET_REFRESH_TOKEN,
+});
+export { authService };
