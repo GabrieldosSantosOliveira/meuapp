45974f863b1f04ad03ae8df5694151026daacc1c
Gabriel dos Santos Oliveira <gabrielsantosoliveira951@gmail.com>
2023-04-02 11:39:29 -0400

    feat: add props id, updatedAt and createdAt in entitie Content

--- src/app/entities/content.ts
+++ src/app/entities/content.ts
@@ -1,11 +1,39 @@
+import { Replace } from '@/helpers/index';
+import { randomUUID } from 'crypto';
+
 export interface ContentProps {
+  id: string;
   heading?: string;
   text: string;
+  updatedAt: Date;
+  createdAt: Date;
 }
 export class Content {
   private props: ContentProps;
-  constructor(props: ContentProps) {
-    this.props = props;
+  constructor(
+    props: Replace<
+      ContentProps,
+      { updatedAt?: Date; createdAt?: Date; id?: string }
+    >,
+  ) {
+    this.props = {
+      id: randomUUID(),
+      createdAt: new Date(),
+      updatedAt: new Date(),
+      ...props,
+    };
+  }
+  public get id(): string {
+    return this.props.id;
+  }
+  public get updatedAt(): Date {
+    return this.props.updatedAt;
+  }
+  public set updatedAt(value: Date) {
+    this.props.updatedAt = value;
+  }
+  public get createdAt(): Date {
+    return this.props.createdAt;
   }
   public get heading(): string | undefined {
     return this.props.heading;
