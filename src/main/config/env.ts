d1c9a987112aa35ebbcbe0852a4f0ef5e76943ef
Gabriel dos Santos Oliveira <gabrielsantosoliveira951@gmail.com>
2023-04-02 18:48:42 -0400

    change default PORT for 3000

--- src/main/config/env.ts
+++ src/main/config/env.ts
@@ -1,6 +1,6 @@
 import 'dotenv/config';
 export const env = {
-  PORT: Number(process.env.PORT) || 3333,
+  PORT: Number(process.env.PORT) || 3000,
   SECRET_ACCESS_TOKEN: process.env.SECRET_ACCESS_TOKEN || 'any_access_token',
   SECRET_REFRESH_TOKEN: process.env.SECRET_REFRESH_TOKEN || 'any_refresh_token',
 };
