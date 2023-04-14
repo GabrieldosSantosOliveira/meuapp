95fe69946bf8fb01e8f9fb70300b455ded2948ed
Gabriel dos Santos Oliveira <gabrielsantosoliveira951@gmail.com>
2023-04-14 15:32:44 -0400

    test: add repository InMemoryCategoryRepository

---
+++ test/repositories/in-memory-category-repository.ts
@@ -0,0 +1,22 @@
+import { Category } from '@/app/entities';
+import {
+  CreateCategoryRepository,
+  LoadCategoryByTitleRepository,
+} from '@/app/repositories';
+
+export class InMemoryCategoryRepository
+  implements LoadCategoryByTitleRepository, CreateCategoryRepository
+{
+  private categorys: Category[] = [];
+
+  async create(category: Category): Promise<void> {
+    this.categorys.push(category);
+  }
+  async findByTitle(title: string): Promise<Category | null> {
+    const category = this.categorys.find(({ title }) => title === title);
+    if (!category) {
+      return null;
+    }
+    return category;
+  }
+}
