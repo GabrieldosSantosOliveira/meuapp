f293b4a98c782a0b863dbc6855ea93b4f85fdaa5
Gabriel dos Santos Oliveira <gabrielsantosoliveira951@gmail.com>
2023-04-16 17:10:36 -0400

    remove folder validations

--- src/interface/validations/index.ts
+++
@@ -1,3 +0,0 @@
-export * from './email-validator';
-export * from './validate-missing-params';
-export * from './url-validator';
--- src/validations/url-validator.ts
+++
@@ -1,8 +0,0 @@
-import validator from 'validator';
-
-import { UrlValidator } from '../interface';
-export class UrlValidatorAdapter implements UrlValidator {
-  async isValid(url: string): Promise<boolean> {
-    return validator.isURL(url);
-  }
-}
