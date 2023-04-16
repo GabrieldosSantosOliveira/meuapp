71fdcbe31a92f27b06248ba22fe314edef06931b
Gabriel dos Santos Oliveira <gabrielsantosoliveira951@gmail.com>
2023-04-16 18:12:48 -0400

    feat: used class-validator to validate the request body

--- src/infra/http/controller/update-author-controller.spec.ts
+++ src/infra/http/controller/update-author-controller.spec.ts
@@ -1,18 +1,16 @@
-import { HttpResponse } from '@/helpers/http';
+import { HttpStatus } from '@/helpers/http';
 import { IHttpRequest } from '@/interface/http';
 import {
   IUpdateAuthorUseCase,
   IUpdateAuthorUseCaseParams,
 } from '@/interface/use-cases';
-import { EmailValidator, UrlValidator } from '@/interface/validations';
-import { InvalidParamError } from '@/utils/http';
 
 import {
   UpdateAuthorController,
   UpdateAuthorControllerRequest,
 } from './update-author-controller';
 class UpdateAuthorUseCaseSpy implements IUpdateAuthorUseCase {
-  data!: IUpdateAuthorUseCaseParams;
+  public data: IUpdateAuthorUseCaseParams;
   async handle(data: IUpdateAuthorUseCaseParams): Promise<void> {
     this.data = data;
   }
@@ -21,40 +19,14 @@ const makeUpdateAuthorUseCaseSpy = () => {
   const updateAuthorUseCaseSpy = new UpdateAuthorUseCaseSpy();
   return { updateAuthorUseCaseSpy };
 };
-class UrlValidatorSpy implements UrlValidator {
-  public isValidUrl = true;
-  async isValid(): Promise<boolean> {
-    return this.isValidUrl;
-  }
-}
-const makeUrlValidatorSpy = () => {
-  const urlValidatorSpy = new UrlValidatorSpy();
-  return { urlValidatorSpy };
-};
-const makeEmailValidatorSpy = () => {
-  class EmailValidatorSpy implements EmailValidator {
-    isEmailValid!: boolean;
-    isValid(): boolean {
-      return this.isEmailValid;
-    }
-  }
-  const emailValidatorSpy = new EmailValidatorSpy();
-  emailValidatorSpy.isEmailValid = true;
-  return { emailValidatorSpy };
-};
+
 const makeSut = () => {
-  const { emailValidatorSpy } = makeEmailValidatorSpy();
-  const { urlValidatorSpy } = makeUrlValidatorSpy();
   const { updateAuthorUseCaseSpy } = makeUpdateAuthorUseCaseSpy();
   const sut = new UpdateAuthorController({
-    emailValidator: emailValidatorSpy,
-    urlValidator: urlValidatorSpy,
     updateAuthorUseCase: updateAuthorUseCaseSpy,
   });
   return {
     sut,
-    emailValidatorSpy,
-    urlValidatorSpy,
     updateAuthorUseCaseSpy,
   };
 };
@@ -64,7 +36,7 @@ const makeRequest = (
   body: {
     firstName: 'any_firstName',
     lastName: 'any_lastName',
-    picture: 'any_picture',
+    picture: 'https://google.com',
     ...params,
   },
   user: {
@@ -80,11 +52,7 @@ describe('UpdateAuthorController', () => {
         firstName: 'any',
       }),
     );
-    expect(httpResponse).toEqual(
-      HttpResponse.badRequest(
-        new InvalidParamError('firstName must be between 5 and 255 characters'),
-      ),
-    );
+    expect(httpResponse.statusCode).toEqual(HttpStatus.BAD_REQUEST);
   });
   it('should return 400 if lastName lastName less than 5 characters or more than 255 characters', async () => {
     const { sut } = makeSut();
@@ -93,20 +61,15 @@ describe('UpdateAuthorController', () => {
         lastName: 'any',
       }),
     );
-    expect(httpResponse).toEqual(
-      HttpResponse.badRequest(
-        new InvalidParamError('lastName must be between 5 and 255 characters'),
-      ),
-    );
+    expect(httpResponse.statusCode).toEqual(HttpStatus.BAD_REQUEST);
   });
 
   it('should return 400 if a invalid url picture is provided', async () => {
-    const { sut, urlValidatorSpy } = makeSut();
-    urlValidatorSpy.isValidUrl = false;
-    const httpResponse = await sut.handle(makeRequest({}));
-    expect(httpResponse).toEqual(
-      HttpResponse.badRequest(new InvalidParamError('picture need be a url')),
+    const { sut } = makeSut();
+    const httpResponse = await sut.handle(
+      makeRequest({ picture: 'invalid_picture' }),
     );
+    expect(httpResponse.statusCode).toEqual(HttpStatus.BAD_REQUEST);
   });
   it('should call updateAuthorUseCase with correct params', async () => {
     const { sut, updateAuthorUseCaseSpy } = makeSut();
@@ -115,5 +78,10 @@ describe('UpdateAuthorController', () => {
       ...makeRequest().body,
       user: { sub: 'any_sub' },
     });
+  });
+  it('should return 204 if success', async () => {
+    const { sut } = makeSut();
+    const httpResponse = await sut.handle(makeRequest());
+    expect(httpResponse.statusCode).toEqual(HttpStatus.NO_CONTENT);
   });
 });
--- src/infra/http/controller/update-author-controller.ts
+++ src/infra/http/controller/update-author-controller.ts
@@ -1,10 +1,13 @@
 import { HttpResponse } from '@/helpers/http';
-import { Controller } from '@/interface/controller';
-import { IHttpRequest, IHttpResponse } from '@/interface/http';
-import { IUpdateAuthorUseCase } from '@/interface/use-cases';
-import { EmailValidator, UrlValidator } from '@/interface/validations';
-import { InvalidParamError } from '@/utils/http';
+import {
+  IUpdateAuthorUseCase,
+  IHttpRequest,
+  IHttpResponse,
+  Controller,
+} from '@/interface/index';
+import { validate } from 'class-validator';
 
+import { UpdateAuthorBodyDto } from '../dtos/update-author-body.dto';
 import { ExceptionFilter } from '../error';
 
 export interface UpdateAuthorControllerRequest {
@@ -13,8 +16,6 @@ export interface UpdateAuthorControllerRequest {
   picture?: string;
 }
 export interface UpdateAuthorControllerParams {
-  emailValidator: EmailValidator;
-  urlValidator: UrlValidator;
   updateAuthorUseCase: IUpdateAuthorUseCase;
 }
 export class UpdateAuthorController implements Controller {
@@ -26,62 +27,21 @@ export class UpdateAuthorController implements Controller {
       if (!request.user?.sub) {
         return HttpResponse.serverError();
       }
-      const validations = {
-        firstName(firstName: string) {
-          if (firstName.length < 5 || firstName.length > 255) {
-            return HttpResponse.badRequest(
-              new InvalidParamError(
-                'firstName must be between 5 and 255 characters',
-              ),
-            );
-          }
-        },
-        lastName(lastName: string) {
-          if (lastName.length < 5 || lastName.length > 255) {
-            return HttpResponse.badRequest(
-              new InvalidParamError(
-                'lastName must be between 5 and 255 characters',
-              ),
-            );
-          }
-        },
-        email(email: string) {
-          if (email.length < 5 || email.length > 255) {
-            return HttpResponse.badRequest(
-              new InvalidParamError(
-                'email must be between 5 and 255 characters',
-              ),
-            );
-          }
-        },
-      };
-      if (request.body.firstName) {
-        const isInvalid = validations.firstName(request.body.firstName);
-        if (isInvalid) return isInvalid;
+      const updateAuthorBodyDto = new UpdateAuthorBodyDto();
+      Object.assign(updateAuthorBodyDto, request.body);
+      const hasErrors = await validate(updateAuthorBodyDto);
+      if (hasErrors.length > 0) {
+        return HttpResponse.badRequest(hasErrors);
       }
-      if (request.body.lastName) {
-        const isInvalid = validations.lastName(request.body.lastName);
-        if (isInvalid) return isInvalid;
-      }
-
-      if (request.body.picture) {
-        const isValidUrl = await this.params.urlValidator.isValid(
-          request.body.picture,
-        );
-        if (!isValidUrl)
-          return HttpResponse.badRequest(
-            new InvalidParamError('picture need be a url'),
-          );
-      }
-
       await this.params.updateAuthorUseCase.handle({
         user: { sub: request.user['sub'] },
-        firstName: request.body.firstName,
-        lastName: request.body.lastName,
-        picture: request.body.picture,
+        firstName: updateAuthorBodyDto.firstName,
+        lastName: updateAuthorBodyDto.lastName,
+        picture: updateAuthorBodyDto.picture,
       });
       return HttpResponse.noContent();
     } catch (error) {
+      console.log(error);
       return ExceptionFilter.handle(error);
     }
   }
