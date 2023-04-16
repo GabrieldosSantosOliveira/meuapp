93154496892da31624554e98a643c78a5f3a7479
Gabriel dos Santos Oliveira <gabrielsantosoliveira951@gmail.com>
2023-04-16 18:16:27 -0400

    feat: add controller CreateNoticeController

---
+++ src/infra/http/controller/notice/create-notice-controller.spec.ts
@@ -0,0 +1,72 @@
+import { Notice } from '@/app/entities';
+import { HttpStatus } from '@/helpers/http';
+import { ICreateNoticeUseCase, IHttpRequest } from '@/interface/index';
+import { makeNotice } from '@/test/factories';
+
+import {
+  CreateNoticeController,
+  CreateNoticeControllerRequest,
+  Content,
+} from './create-notice-controller';
+class CreateNoticeUseCaseSpy implements ICreateNoticeUseCase {
+  async handle(): Promise<Notice> {
+    return makeNotice();
+  }
+}
+const makeCreateNoticeUseCaseSpy = () => {
+  const createNoticeUseCaseSpy = new CreateNoticeUseCaseSpy();
+  return { createNoticeUseCaseSpy };
+};
+const makeSut = () => {
+  const { createNoticeUseCaseSpy } = makeCreateNoticeUseCaseSpy();
+  const sut = new CreateNoticeController({
+    createNoticeUseCase: createNoticeUseCaseSpy,
+  });
+  return { sut };
+};
+const makeRequest = (
+  params: Partial<CreateNoticeControllerRequest> = {},
+): IHttpRequest<CreateNoticeControllerRequest> => {
+  return {
+    body: {
+      description: 'any_description',
+      title: 'any_description',
+      content: [{ text: 'lorem ipsum', heading: 'any_heading' }],
+      image: 'any_image',
+      category: 'any_category',
+      ...params,
+    },
+    user: { sub: 'any_id' },
+  };
+};
+describe('CreateNoticeController', () => {
+  it('should return 400 if title is not provided', async () => {
+    const { sut } = makeSut();
+    const httpResponse = await sut.handle(makeRequest({ title: undefined }));
+    expect(httpResponse.statusCode).toEqual(HttpStatus.BAD_REQUEST);
+  });
+  it('should return 400 if title is empty', async () => {
+    const { sut } = makeSut();
+    const httpResponse = await sut.handle(makeRequest({ title: ' ' }));
+    expect(httpResponse.statusCode).toEqual(HttpStatus.BAD_REQUEST);
+  });
+  it('should return 400 if content is empty', async () => {
+    const { sut } = makeSut();
+    const httpResponse = await sut.handle(makeRequest({ content: [] }));
+    expect(httpResponse.statusCode).toEqual(HttpStatus.BAD_REQUEST);
+  });
+  it('should return 400 if text in content is empty', async () => {
+    const { sut } = makeSut();
+    const httpResponse = await sut.handle(
+      makeRequest({ content: [{ text: ' ' }] }),
+    );
+    expect(httpResponse.statusCode).toEqual(HttpStatus.BAD_REQUEST);
+  });
+  it('should return 400 if no has text in content', async () => {
+    const { sut } = makeSut();
+    const httpResponse = await sut.handle(
+      makeRequest({ content: [{} as Content, {} as Content] }),
+    );
+    expect(httpResponse.statusCode).toEqual(HttpStatus.BAD_REQUEST);
+  });
+});
---
+++ src/infra/http/controller/notice/create-notice-controller.ts
@@ -0,0 +1,62 @@
+import { HttpResponse } from '@/helpers/http';
+import { Controller } from '@/interface/controller';
+import { IHttpRequest, IHttpResponse } from '@/interface/http';
+import { ICreateNoticeUseCase } from '@/interface/use-cases/create-notice-use-case';
+import { validate } from 'class-validator';
+
+import {
+  CreateContentBodyDto,
+  CreateNoticeBodyDto,
+} from '../../dtos/create-notice-body.dto';
+import { ExceptionFilter } from '../../error';
+import { NoticeViewModel } from '../../view-models/notice-view-model';
+export interface CreateNoticeControllerRequest {
+  content: Content[];
+  title: string;
+  description: string;
+  image: string;
+  category: string;
+}
+export interface Content {
+  heading?: string;
+  text: string;
+}
+export interface CreateNoticeControllerConstructorParams {
+  createNoticeUseCase: ICreateNoticeUseCase;
+}
+export class CreateNoticeController implements Controller {
+  constructor(
+    private readonly params: CreateNoticeControllerConstructorParams,
+  ) {}
+  async handle(
+    request: IHttpRequest<CreateNoticeControllerRequest>,
+  ): Promise<IHttpResponse> {
+    try {
+      if (!request.user) {
+        return HttpResponse.serverError();
+      }
+      const createNoticeBodyDto = new CreateNoticeBodyDto();
+      createNoticeBodyDto.category = request.body.category;
+      createNoticeBodyDto.description = request.body.description;
+      createNoticeBodyDto.image = request.body.image;
+      createNoticeBodyDto.title = request.body.title;
+      const createContentBodyDtos = request.body.content.map((content) => {
+        const createContentBodyDto = new CreateContentBodyDto();
+        Object.assign(createContentBodyDto, content);
+        return createContentBodyDto;
+      });
+      createNoticeBodyDto.content = createContentBodyDtos;
+      const hasErrors = await validate(createNoticeBodyDto);
+      if (hasErrors.length > 0) {
+        return HttpResponse.badRequest(hasErrors);
+      }
+      const notice = await this.params.createNoticeUseCase.handle({
+        user: request.user,
+        ...createNoticeBodyDto,
+      });
+      return HttpResponse.created(NoticeViewModel.toHTTP(notice));
+    } catch (error) {
+      return ExceptionFilter.handle(error);
+    }
+  }
+}
