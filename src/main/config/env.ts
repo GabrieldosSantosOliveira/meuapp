07dd8aac77bdf07ce2e733124d8d756fe2a8d3d4
Gabriel dos Santos Oliveira <gabrielsantosoliveira951@gmail.com>
2023-04-17 20:37:25 -0400

    feat: add new variables

--- src/main/config/env.ts
+++ src/main/config/env.ts
@@ -1,6 +1,9 @@
 import 'dotenv/config';
+const PORT = 3000;
 export const env = {
-  PORT: Number(process.env.PORT) || 3000,
+  PORT: Number(process.env.PORT) || PORT,
   SECRET_ACCESS_TOKEN: process.env.SECRET_ACCESS_TOKEN || 'any_access_token',
   SECRET_REFRESH_TOKEN: process.env.SECRET_REFRESH_TOKEN || 'any_refresh_token',
+  BASE_URL: process.env.BASE_URL || `http://localhost:${PORT}`,
+  SIZE_FOR_PAGE: Number(process.env.BASE_URL) || 20,
 };
