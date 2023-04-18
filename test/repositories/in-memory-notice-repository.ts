7f0c87fbe689cd74fde10f2227fd481f15eb181c
Gabriel dos Santos Oliveira <gabrielsantosoliveira951@gmail.com>
2023-04-17 20:31:07 -0400

    test: implements CountNoticeRepository in InMemoryNoticeRepository

--- test/repositories/in-memory-notice-repository.ts
+++ test/repositories/in-memory-notice-repository.ts
@@ -1,17 +1,30 @@
 import { Notice } from '@/app/entities';
 import {
+  CountNoticeRepository,
   CreateNoticeRepository,
   LoadAllNoticeRepository,
+  LoadAllNoticeRepositoryOptions,
 } from '@/app/repositories';
 
 export class InMemoryNoticeRepository
-  implements LoadAllNoticeRepository, CreateNoticeRepository
+  implements
+    LoadAllNoticeRepository,
+    CreateNoticeRepository,
+    CountNoticeRepository
 {
   private notices: Notice[] = [];
   async create(notice: Notice): Promise<void> {
     this.notices.push(notice);
   }
-  async findAll(): Promise<Notice[]> {
-    return this.notices;
+  async findAllByPage(
+    options: LoadAllNoticeRepositoryOptions,
+  ): Promise<Notice[]> {
+    const PAGE = Math.max(options.page, 1);
+    const SIZE_FOR_PAGE = options.sizeForPage;
+    const SKIP = Math.max((PAGE - 1) * SIZE_FOR_PAGE, 0);
+    return this.notices.splice(SKIP, SKIP + SIZE_FOR_PAGE);
+  }
+  async count(): Promise<number> {
+    return this.notices.length;
   }
 }
