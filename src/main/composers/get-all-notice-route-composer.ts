ace1c63f61964d83427cb8f38bb49e828ddca5d3
Gabriel dos Santos Oliveira <gabrielsantosoliveira951@gmail.com>
2023-04-17 20:26:43 -0400

    feat: add route-composer GetAllNoticeRouterComposer

---
+++ src/main/composers/get-all-notice-route-composer.ts
@@ -0,0 +1,28 @@
+import { GetAllNoticeUseCase } from '@/app/use-cases';
+import { PrismaCountNoticeRepository } from '@/infra/database/prisma';
+import { PrismaLoadAllNoticeRepository } from '@/infra/database/prisma';
+import { GetAllNoticeController } from '@/infra/http/controller';
+
+import { env } from '../config';
+import { prismaService } from '../lib';
+
+export class GetAllNoticeRouterComposer {
+  public static route() {
+    const prismaLoadAllNoticeRepository = new PrismaLoadAllNoticeRepository(
+      prismaService,
+    );
+    const prismaCountNoticeRepository = new PrismaCountNoticeRepository(
+      prismaService,
+    );
+    const getAllNoticeUseCase = new GetAllNoticeUseCase({
+      loadAllNoticeRepository: prismaLoadAllNoticeRepository,
+      countNoticeRepository: prismaCountNoticeRepository,
+    });
+    const getAllNoticeController = new GetAllNoticeController({
+      getAllNoticeUseCase: getAllNoticeUseCase,
+      BASE_URL: env.BASE_URL,
+      SIZE_FOR_PAGE: env.SIZE_FOR_PAGE,
+    });
+    return { getAllNoticeController };
+  }
+}
