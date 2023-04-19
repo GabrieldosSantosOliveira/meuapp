36fdfc4943aec95bc6d0210a0ef6bdce168f0b3e
Gabriel dos Santos Oliveira <gabrielsantosoliveira951@gmail.com>
2023-04-18 22:41:22 -0400

    feat: add repository PrismaLoadAllNoticeByCategoryRepository

---
+++ src/infra/database/prisma/repositories/notice/prisma-load-all-notice-by-category-repository.ts
@@ -0,0 +1,37 @@
+import { Notice } from '@/app/entities';
+import {
+  LoadAllNoticeByCategoryRepository,
+  LoadAllNoticeByCategoryRepositoryOptions,
+} from '@/app/repositories';
+
+import { PrismaCategoryMapper, PrismaNoticeMapper } from '../../mappers';
+import { PrismaService } from '../../prisma-service';
+
+export class PrismaLoadAllNoticeByCategoryRepository
+  implements LoadAllNoticeByCategoryRepository
+{
+  constructor(private readonly prismaService: PrismaService) {}
+  async findAllByCategoryByPage(
+    options: LoadAllNoticeByCategoryRepositoryOptions,
+  ): Promise<Notice[]> {
+    const SKIP = Math.max((options.page - 1) * options.sizeForPage, 0);
+    const rawCategory = PrismaCategoryMapper.toPrisma(options.category);
+    const rawNotices = await this.prismaService.notice.findMany({
+      include: { Author: true, Category: true, Content: true },
+      take: options.sizeForPage,
+      skip: SKIP,
+      where: {
+        categoryId: rawCategory.id,
+      },
+    });
+
+    return rawNotices.map((rawNotice) =>
+      PrismaNoticeMapper.toDomain({
+        rawAuthor: rawNotice.Author,
+        rawCategory: rawNotice.Category,
+        rawContent: rawNotice.Content,
+        rawNotice: rawNotice,
+      }),
+    );
+  }
+}
