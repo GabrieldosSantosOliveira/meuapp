3ad4d4a8be02764091975231bb963c86bfc76724
Gabriel dos Santos Oliveira <gabrielsantosoliveira951@gmail.com>
2023-04-18 23:13:08 -0400

    refactor: added promises directly to promise.all

--- src/app/use-cases/notice/get-all-notice-use-case.ts
+++ src/app/use-cases/notice/get-all-notice-use-case.ts
@@ -17,21 +17,22 @@ export class GetAllNoticeUseCase implements IGetAllNoticeUseCase {
   async handle(
     options: IGetAllNoticeUseCaseOptions,
   ): Promise<IGetAllNoticeUseCaseResponse> {
+    const { BASE_URL, SIZE_FOR_PAGE, page } = options;
     let next: string | null = null;
     let prev: string | null = null;
-    const { BASE_URL, SIZE_FOR_PAGE, page } = options;
-    const findAllByPage = this.params.loadAllNoticeRepository.findAllByPage({
-      page,
-      sizeForPage: SIZE_FOR_PAGE,
-    });
-    const count = this.params.countNoticeRepository.count();
-    const [notices, countOfNotices] = await Promise.all([findAllByPage, count]);
+    const [notices, countOfNotices] = await Promise.all([
+      this.params.loadAllNoticeRepository.findAllByPage({
+        page,
+        sizeForPage: SIZE_FOR_PAGE,
+      }),
+      this.params.countNoticeRepository.count(),
+    ]);
     const pages = Math.ceil(countOfNotices / SIZE_FOR_PAGE);
     if (page - 1 > 0) {
-      prev = new URL(`?page=${page - 1}`, BASE_URL).toString();
+      prev = new URL(`/api/notice?page=${page - 1}`, BASE_URL).toString();
     }
     if (page < pages) {
-      next = new URL(`?page=${page + 1}`, BASE_URL).toString();
+      next = new URL(`/api/notice?page=${page + 1}`, BASE_URL).toString();
     }
     if (page > pages) prev = null;
     const info: Info = {
