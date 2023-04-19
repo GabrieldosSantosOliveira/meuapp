8bff0064ee3c89743994d4ecdaf3eac19556adef
Gabriel dos Santos Oliveira <gabrielsantosoliveira951@gmail.com>
2023-04-18 22:45:56 -0400

    feat: implement interfaces CountNoticeByCategoryRepository and LoadAllNoticeByCategoryRepository in InMemoryNoticeRepository

--- test/repositories/in-memory-notice-repository.ts
+++ test/repositories/in-memory-notice-repository.ts
@@ -1,7 +1,10 @@
-import { Notice } from '@/app/entities';
+import { Category, Notice } from '@/app/entities';
 import {
+  CountNoticeByCategoryRepository,
   CountNoticeRepository,
   CreateNoticeRepository,
+  LoadAllNoticeByCategoryRepository,
+  LoadAllNoticeByCategoryRepositoryOptions,
   LoadAllNoticeRepository,
   LoadAllNoticeRepositoryOptions,
 } from '@/app/repositories';
@@ -10,9 +13,25 @@ export class InMemoryNoticeRepository
   implements
     LoadAllNoticeRepository,
     CreateNoticeRepository,
-    CountNoticeRepository
+    CountNoticeRepository,
+    CountNoticeByCategoryRepository,
+    LoadAllNoticeByCategoryRepository
 {
-  private notices: Notice[] = [];
+  public notices: Notice[] = [];
+  async findAllByCategoryByPage(
+    options: LoadAllNoticeByCategoryRepositoryOptions,
+  ): Promise<Notice[]> {
+    const PAGE = Math.max(options.page, 1);
+    const SIZE_FOR_PAGE = options.sizeForPage;
+    const SKIP = Math.max((PAGE - 1) * SIZE_FOR_PAGE, 0);
+    return this.notices
+      .filter((notice) => notice.category.id === options.category.id)
+      .splice(SKIP, SKIP + SIZE_FOR_PAGE);
+  }
+  async countByCategory(category: Category): Promise<number> {
+    return this.notices.filter((notice) => notice.category.id === category.id)
+      .length;
+  }
   async create(notice: Notice): Promise<void> {
     this.notices.push(notice);
   }
@@ -22,7 +41,9 @@ export class InMemoryNoticeRepository
     const PAGE = Math.max(options.page, 1);
     const SIZE_FOR_PAGE = options.sizeForPage;
     const SKIP = Math.max((PAGE - 1) * SIZE_FOR_PAGE, 0);
-    return this.notices.splice(SKIP, SKIP + SIZE_FOR_PAGE);
+    return this.notices
+      .filter((notice) => Boolean(notice))
+      .splice(SKIP, SKIP + SIZE_FOR_PAGE);
   }
   async count(): Promise<number> {
     return this.notices.length;
